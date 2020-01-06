5e73745f994c059c1d6d244c7888714dcc126704
Bruce Holway <20.bruce.holway@ksd.org>
2020-01-06 10:26:15 -0800

    testing levels

---
+++ RamioGroupProjectPlatformer/Assets/Scripts/PlatformerMovement.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlatformerMovement : MonoBehaviour
+{
+    public float moveSpeed = 1.0f;
+    public float jumpSpeed = 1.0f;
+    bool grounded = false;
+    Animator anim;
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+    
+    void Update()
+    {
+        float moveX = Input.GetAxis("Horizontal");
+        Vector2 velocity = GetComponent<Rigidbody2D>().velocity;
+        velocity.x = moveX * moveSpeed;
+        GetComponent<Rigidbody2D>().velocity = velocity;
+        if(Input.GetButtonDown("Jump") && grounded)
+        {
+            Jump();
+        }
+        anim.SetBool("grounded", grounded);
+        anim.SetFloat("x", velocity.x);
+        anim.SetFloat("y", velocity.y);
+        float x = Input.GetAxisRaw("Horizontal");
+        if(x > 0)
+        {
+            GetComponent<SpriteRenderer>().flipX = false;
+        }else if (x < 0)
+        {
+            GetComponent<SpriteRenderer>().flipX = true;
+        }
+    }
+    public void Jump()
+    {
+        GetComponent<Rigidbody2D>().AddForce(new Vector2(0, 100 * jumpSpeed));
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.gameObject.layer == 0)
+        {
+            grounded = true;
+        }
+    }
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        if (collision.gameObject.layer == 0)
+        {
+            grounded = true;
+        }
+    }
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if(collision.gameObject.layer == 0)
+        {
+            grounded = false;
+        }
+    }
+}
